6b0ae26fb263f53fa1e43f7867332cc2197bc3b3
Srikanth Srungarapu <srikanth235@gmail.com>
2024-03-11 17:08:16 +0530

    fix: adding responsiveness to home page.

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,8 @@ import { Search } from "@/components/search"
 export default function IndexPage() {
   return (
     // Ensuring the main container allows for full height alignment
-    <div className="container mx-auto flex w-full flex-1 items-center justify-center">
-      <section className=" flex min-h-full flex-col items-center justify-center p-4 pb-10">
-        <Search />
-      </section>
+    <div className="container mx-auto flex w-full flex-1 items-center justify-center p-2 pb-8">
+      <Search />
     </div>
   )
 }
