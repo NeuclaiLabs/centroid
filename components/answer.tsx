0113e1f467485e5fd92218f45c7035e58704886c
Srikanth Srungarapu <srikanth235@gmail.com>
2024-03-20 13:47:16 +0530

    feat: adding support for rendering codeblock and autoscroll

--- components/answer.tsx
+++ components/answer.tsx
@@ -1,10 +1,20 @@
 import { Message } from "ai/react"
+import ReactMarkdown from "react-markdown"
+import remarkGfm from "remark-gfm"
+import remarkMath from "remark-math"
 
 import { Skeleton } from "@/components/ui/skeleton"
 import { ButtonScrollToBottom } from "@/components/button-scroll-to-bottom"
+import { CodeBlock } from "@/components/codeblock"
 import { ButtonGroup } from "@/components/conversation-actions"
 import { Suggestions } from "@/components/suggestions"
 
+interface CodeProps {
+  node?: any
+  inline?: any
+  className?: any
+  children?: any
+}
 export function Answer({
   message,
   isLoading,
@@ -18,7 +28,52 @@ export function Answer({
         <div className="rounded-md">
           <ButtonScrollToBottom />
           <h2 className="pb-2 font-bold uppercase">Answer</h2>
-          <p className="overflow-wrap text-base">{message.content}</p>
+          <ReactMarkdown
+            className="prose dark:prose-invert prose-p:leading-relaxed prose-pre:p-0 break-words"
+            remarkPlugins={[remarkGfm, remarkMath]}
+            components={{
+              p({ children }) {
+                return <p className="mb-2 last:mb-0">{children}</p>
+              },
+              code({ node, className, children, ...props }: CodeProps) {
+                if (children?.length) {
+                  if (children[0] == "▍") {
+                    return (
+                      <span className="mt-1 animate-pulse cursor-default">
+                        ▍
+                      </span>
+                    )
+                  }
+
+                  ;(children[0] as string).replace("`▍`", "▍")
+                }
+
+                const match = /language-(\w+)/.exec(className || "")
+                const codeText = Array.isArray(node.properties.children)
+                  ? node.properties.children.join("")
+                  : ""
+                const numLines = codeText.split("\n").length
+
+                if (!match && numLines < 3) {
+                  return (
+                    <code {...props}>
+                      <b>{children}</b>
+                    </code>
+                  )
+                }
+                return (
+                  <CodeBlock
+                    key={Math.random()}
+                    language={(match && match[1]) || "auto"}
+                    value={String(children).replace(/\n$/, "")}
+                    {...props}
+                  />
+                )
+              },
+            }}
+          >
+            {message.content}
+          </ReactMarkdown>
           <ButtonGroup /> <Suggestions />
         </div>
       </div>
--- components/icons.tsx
+++ components/icons.tsx
@@ -1,12 +1,26 @@
-import { Check, ChevronDown, ChevronUp, ChevronsLeftRight, Circle, Copy, CornerDownLeft, LucideProps, Moon, Pencil, PlusCircle, PlusCircleIcon, RefreshCw, Settings, SunMedium, ThumbsDown, ThumbsUp, Twitter, type Icon as LucideIcon } from "lucide-react";
+import {
+  Check,
+  ChevronDown,
+  ChevronUp,
+  ChevronsLeftRight,
+  Circle,
+  Copy,
+  CornerDownLeft,
+  LucideProps,
+  Moon,
+  Pencil,
+  PlusCircle,
+  PlusCircleIcon,
+  RefreshCw,
+  Settings,
+  SunMedium,
+  ThumbsDown,
+  ThumbsUp,
+  Twitter,
+  type Icon as LucideIcon,
+} from "lucide-react"
 
-
-
-import { cn } from "@/lib/utils";
-
-
-
-
+import { cn } from "@/lib/utils"
 
 export type Icon = LucideIcon
 
@@ -93,8 +107,10 @@ export const Icons = {
   ),
 }
 
-
-export function IconArrowElbow({ className, ...props }: React.ComponentProps<"svg">) {
+export function IconArrowElbow({
+  className,
+  ...props
+}: React.ComponentProps<"svg">) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -139,20 +155,22 @@ export function IconArrowDown({
   )
 }
 
-export function IconRefresh({ className, ...props }: React.ComponentProps<'svg'>) {
+export function IconRefresh({
+  className,
+  ...props
+}: React.ComponentProps<"svg">) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 256 256"
       fill="currentColor"
-      className={cn('size-4', className)}
+      className={cn("size-4", className)}
       {...props}
     >
       <path d="M197.67 186.37a8 8 0 0 1 0 11.29C196.58 198.73 170.82 224 128 224c-37.39 0-64.53-22.4-80-39.85V208a8 8 0 0 1-16 0v-48a8 8 0 0 1 8-8h48a8 8 0 0 1 0 16H55.44C67.76 183.35 93 208 128 208c36 0 58.14-21.46 58.36-21.68a8 8 0 0 1 11.31.05ZM216 40a8 8 0 0 0-8 8v23.85C192.53 54.4 165.39 32 128 32c-42.82 0-68.58 25.27-69.66 26.34a8 8 0 0 0 11.3 11.34C69.86 69.46 92 48 128 48c35 0 60.24 24.65 72.56 40H168a8 8 0 0 0 0 16h48a8 8 0 0 0 8-8V48a8 8 0 0 0-8-8Z" />
     </svg>
   )
 }
-
 
 export function IconStop({ className, ...props }: React.ComponentProps<"svg">) {
   return (
@@ -164,6 +182,54 @@ export function IconStop({ className, ...props }: React.ComponentProps<"svg">) {
       {...props}
     >
       <path d="M128 24a104 104 0 1 0 104 104A104.11 104.11 0 0 0 128 24Zm0 192a88 88 0 1 1 88-88 88.1 88.1 0 0 1-88 88Zm24-120h-48a8 8 0 0 0-8 8v48a8 8 0 0 0 8 8h48a8 8 0 0 0 8-8v-48a8 8 0 0 0-8-8Zm-8 48h-32v-32h32Z" />
+    </svg>
+  )
+}
+
+export function IconCopy({ className, ...props }: React.ComponentProps<"svg">) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 256 256"
+      fill="currentColor"
+      className={cn("size-4", className)}
+      {...props}
+    >
+      <path d="M216 32H88a8 8 0 0 0-8 8v40H40a8 8 0 0 0-8 8v128a8 8 0 0 0 8 8h128a8 8 0 0 0 8-8v-40h40a8 8 0 0 0 8-8V40a8 8 0 0 0-8-8Zm-56 176H48V96h112Zm48-48h-32V88a8 8 0 0 0-8-8H96V48h112Z" />
+    </svg>
+  )
+}
+
+export function IconCheck({
+  className,
+  ...props
+}: React.ComponentProps<"svg">) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 256 256"
+      fill="currentColor"
+      className={cn("size-4", className)}
+      {...props}
+    >
+      <path d="m229.66 77.66-128 128a8 8 0 0 1-11.32 0l-56-56a8 8 0 0 1 11.32-11.32L96 188.69 218.34 66.34a8 8 0 0 1 11.32 11.32Z" />
+    </svg>
+  )
+}
+
+export function IconDownload({
+  className,
+  ...props
+}: React.ComponentProps<"svg">) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 256 256"
+      fill="currentColor"
+      className={cn("size-4", className)}
+      {...props}
+    >
+      <path d="M224 152v56a16 16 0 0 1-16 16H48a16 16 0 0 1-16-16v-56a8 8 0 0 1 16 0v56h160v-56a8 8 0 0 1 16 0Zm-101.66 5.66a8 8 0 0 0 11.32 0l40-40a8 8 0 0 0-11.32-11.32L136 132.69V40a8 8 0 0 0-16 0v92.69l-26.34-26.35a8 8 0 0 0-11.32 11.32Z" />
     </svg>
   )
 }
