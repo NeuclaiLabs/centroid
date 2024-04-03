61f5b82efe2e1b2f96d1ed66759369842ae76662
Srikanth Srungarapu <srikanth235@gmail.com>
2024-04-03 13:43:30 +0530

    feat: refactoring header

--- frontend/components/theme-toggle.tsx
+++ frontend/components/theme-toggle.tsx
@@ -6,6 +6,7 @@ import { useTheme } from 'next-themes'
 import { Button } from '@/components/ui/button'
 import { IconMoon, IconSun } from '@/components/ui/icons'
 
+
 export function ThemeToggle() {
   const { setTheme, theme } = useTheme()
   const [_, startTransition] = React.useTransition()
