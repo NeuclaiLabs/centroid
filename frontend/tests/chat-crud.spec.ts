4e83f585fa797e14ed8c1418b70d95444a966912
Srikanth Srungarapu <srikanth235@gmail.com>
2024-04-10 16:45:07 +0530

    feat: adding crud tests for chat page

--- frontend/tests/chat-crud.spec.ts
+++ frontend/tests/chat-crud.spec.ts
@@ -1,25 +1,98 @@
-import { test, expect } from '@playwright/test'
+import { test, expect, type Page } from '@playwright/test'
 
-test('chat-create', async ({ page }) => {
-  let chatText = 'hi'
+test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:3000/')
-  await page.getByPlaceholder('Send a message.').fill('hi\n')
+})
+
+const MESSAGES = ['hi', 'what is 20 plus 30']
+test('create a new chat', async ({ page }) => {
+  let regex = new RegExp(`^${MESSAGES[0]}$`)
+  // Send first message
+  await page.getByPlaceholder('Send a message.').fill(MESSAGES[0])
+  await page.getByRole('button', { name: 'Send message' }).click()
+
+  // Checking first user message
+  await expect(
+    page.locator('div').filter({ hasText: regex }).first()
+  ).toBeVisible()
+
+  // Check that send message input field is empty
+  await expect(page.getByPlaceholder('Send a message.')).toBeEmpty()
+
+  // Checking first bot reply message
+  await expect(
+    page
+      .getByRole('main')
+      .getByRole('img', { name: 'OpenAstra' })
+      .locator('path')
+      .nth(1)
+  ).toBeVisible()
+  await expect(
+    page
+      .locator('div')
+      .filter({ hasText: /^.*assist.*$/ })
+      .first()
+  ).toBeVisible()
+})
+
+test('add more messages to the chat', async ({ page }) => {
+  let regex = new RegExp(`^${MESSAGES[1]}$`)
+
+  await createDefaultChat(page)
+  await page.waitForURL('**/chat/**')
+
+  // Send second message
+  await page.getByPlaceholder('Send a message.').fill(MESSAGES[1])
+  await page.getByRole('button', { name: 'Send message' }).click()
+
+  // Checking second user message
+  await expect(
+    page.locator('div').filter({ hasText: regex }).first()
+  ).toBeVisible()
+
+  // Checking second bot reply
+  await expect(
+    page
+      .getByRole('main')
+      .getByRole('img', { name: 'OpenAstra' })
+      .locator('path')
+      .nth(2)
+  ).toBeVisible()
+  await expect(
+    page
+      .locator('div')
+      .filter({ hasText: /^.*20.*30.*50.*$/ })
+      .first()
+  ).toBeVisible()
+})
+
+test('delete a chat', async ({ page }) => {
+  await createDefaultChat(page)
+  await page.waitForURL('**/chat/**')
+  await page.getByRole('button', { name: 'Delete' }).click()
+  page.on('dialog', dialog => dialog.accept())
+  await page.getByRole('button', {name: "Delete"}).click()
+  await expect(
+    page.getByText(
+      'Welcome to Next.js AI Chatbot!This is an open source AI chatbot app template'
+    )
+  ).toBeVisible()
+})
+async function createDefaultChat(page: Page) {
+  // create a new todo locator
+  await page.getByPlaceholder('Send a message.').fill(MESSAGES[0])
   await page.getByRole('button', { name: 'Send message' }).click()
   await expect(
     page
       .getByRole('main')
       .getByRole('img', { name: 'OpenAstra' })
       .locator('path')
-      .first()
+      .nth(1)
   ).toBeVisible()
   await expect(
-    page.getByRole('main').getByRole('img', { name: 'OpenAstra' })
+    page
+      .locator('div')
+      .filter({ hasText: /^.*assist.*$/ })
+      .first()
   ).toBeVisible()
-  // await expect(
-  //   page.getByRole('main').getByRole('img', { name: 'UserAvatar' })
-  // ).toContainText('hi')
-  // await page.getByRole('button', { name: 'Delete' }).click();
-  // await page.getByRole('button', { name: 'Delete' }).click();
-  // await expect(page.getByRole('main')).toContainText('Welcome to Next.js AI Chatbot!This is an open source AI chatbot app template built with Next.js, the Vercel AI SDK, and Vercel KV.It uses React Server Components to combine text with generative UI as output of the LLM. The UI state is synced through the SDK so the model is aware of your interactions as they happen.');
-  // await expect(page.getByText('What are thetrending')).toBeVisible();
-})
+}
