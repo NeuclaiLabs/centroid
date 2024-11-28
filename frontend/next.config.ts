80e75d4893642410a87f6b015350bb35a0ab0f8f
Srikanth Srungarapu <srikanth235@gmail.com>
2024-11-28 20:25:42 +0530

    feat: adding docker support for frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   images: {
     remotePatterns: [],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://api.openastra.com/api/:path*",
+      },
+    ];
   },
 };
 export default nextConfig;
