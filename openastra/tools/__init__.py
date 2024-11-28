b34574631285672b24fff0b89a7462f339cd678a
Srikanth Srungarapu <srikanth235@gmail.com>
2024-11-28 16:46:48 +0530

    fix: deleting unwatned files

--- openastra/tools/__init__.py
+++
@@ -1,5 +0,0 @@
-from openastra.tools.calculator.main import CalculatorTool
-
-__all__ = [
-    "CalculatorTool",
-]
--- openastra/utils.py
+++
@@ -1,9 +0,0 @@
-from langchain_core.tools import ToolException
-
-
-def handle_error(error: ToolException) -> str:
-    return (
-        "The following errors occurred during tool execution:"
-        + error.args[0]
-        + "Please try another tool."
-    )
