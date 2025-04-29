8a11316e4e39edf1e41fcd01ccac1ba34e499db0
Srikanth Srungarapu <srikanth235@gmail.com>
2025-04-29 19:30:45 +0530

    fix: adding redirects for docs

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,6 +5,24 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+
+  // Add rewrites to handle the subdomain mapping
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'docs.centroid.run',
+            },
+          ],
+          destination: '/docs/:path*',
+        }
+      ]
+    };
+  },
 };
 
 export default withMDX(config);
